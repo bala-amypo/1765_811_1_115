8f47c2357cd71ba7ed18d946c9a92d0160e2f437
bala-amypo <bala@amypo.in>
2025-12-18 18:37:46 +0000

    User commit

--- src/main/java/com/example/demo/service/BreachDetectionService.java
+++ src/main/java/com/example/demo/service/BreachDetectionService.java
@@ -1,3 +1,8 @@
+package com.example.demo.service;
+
+import java.util.List;
+import com.example.demo.entity.BreachRecordEntity;
+
 public interface BreachDetectionService {
 
     BreachRecordEntity createBreach(BreachRecordEntity breach);
