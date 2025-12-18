3d9cfadb1a73c4c6de26fc010b38f06b5b61ae16
bala-amypo <bala@amypo.in>
2025-12-18 17:51:01 +0000

    User commit

--- src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
+++ src/main/java/com/example/demo/servlet/SimpleStatusServlet.java
@@ -1,16 +1,16 @@
-package com.example.demo.servlet;
+// package com.example.demo.servlet;
 
-import jakarta.servlet.http.HttpServlet;
-import jakarta.servlet.http.HttpServletRequest;
-import jakarta.servlet.http.HttpServletResponse;
-import jakarta.servlet.annotation.WebServlet;
-import java.io.IOException;
+// import jakarta.servlet.http.HttpServlet;
+// import jakarta.servlet.http.HttpServletRequest;
+// import jakarta.servlet.http.HttpServletResponse;
+// import jakarta.servlet.annotation.WebServlet;
+// import java.io.IOException;
 
-@WebServlet(urlPatterns = "/simple-status")
-public class SimpleStatusServlet extends HttpServlet {
+// @WebServlet(urlPatterns = "/simple-status")
+// public class SimpleStatusServlet extends HttpServlet {
 
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
-        resp.setStatus(200);
-        resp.getWriter().write("Cold Chain Temperature Breach Alert System is running");
-    }
-}
+//     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
+//         resp.setStatus(200);
+//         resp.getWriter().write("Cold Chain Temperature Breach Alert System is running");
+//     }
+// }
