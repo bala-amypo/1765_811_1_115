0a02fac2ca1de869bf0e09826e6e5c179ef00e54
bala-amypo <bala@amypo.in>
2025-12-18 15:57:36 +0000

    User commit

--- src/main/java/com/example/demo/repository/TemperatureSensorLogRepository.java
+++ src/main/java/com/example/demo/repository/TemperatureSensorLogRepository.java
@@ -1,17 +1,10 @@
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-import java.util.Optional;
 
-public interface TemperatureSensorLogRepository
-        extends JpaRepository<TemperatureSensorLogEntity, Long> {
-    List<TemperatureSensorLogEntity> findByShipmentId(Long shipmentId);
-}
 package com.example.demo.repository;
 
 import com.example.demo.entity.AlertRecordEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 
-public interface AlertRecordRepository extends JpaRepository<AlertRecordEntity, Long> {
-    List<AlertRecordEntity> findByShipmentId(Long shipmentId);
+public interface TemperatureSensorLogRepository extends JpaRepository<TemperatureSensorLogRepository, Long> {
+    List<TemperatureSensorLogRepository> findByShipmentId(Long shipmentId);
 }
