6dab67e1253b1d64b8c0b7f130a862a9d520cb2c
bala-amypo <bala@amypo.in>
2025-12-18 08:46:49 +0000

    User commit

--- src/main/java/com/example/demo/service/TemperatureLogService.java
+++ src/main/java/com/example/demo/service/TemperatureLogService.java
@@ -1,24 +1,9 @@
+
 package com.example.demo.service;
 
 import java.util.List;
 
 import com.example.demo.entity.TemperatureLogEntity;
-
-public interface TemperatureLogService {
-
-    TemperatureLogEntity addTemperatureLog(TemperatureLogEntity log);
-
-    List<TemperatureLogEntity> getTemperatureLogs();
-
-    TemperatureLogEntity getTemperatureLogById(Long id);
-
-    void deleteTemperatureLogById(Long id);
-}
-package com.example.demo.service;
-
-import java.util.List;
-
-import com.example.demo.entity.TemperatureSensorLogEntity;
 
 public interface TemperatureLogService {
 
