ecc9571b5b14f4a2dfb972090a89d0314d0518cb
bala-amypo <bala@amypo.in>
2025-12-20 15:58:44 +0000

    User commit

--- src/main/java/com/example/demo/controller/ShipmentRecordController.java
+++ src/main/java/com/example/demo/controller/ShipmentRecordController.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import com.example.demo.entity.ShipmentRecordEntity;
+import com.example.demo.entity.ShipmentRecord;
 import com.example.demo.service.ShipmentRecordService;
 
 @RestController
@@ -16,22 +16,22 @@ public class ShipmentRecordController {
     private ShipmentRecordService service;
 
     @PostMapping
-    public ShipmentRecordEntity addShipment(@RequestBody ShipmentRecordEntity shipment) {
+    public ShipmentRecord addShipment(@RequestBody ShipmentRecord shipment) {
         return service.addShipment(shipment);
     }
 
     @GetMapping
-    public List<ShipmentRecordEntity> getAllShipments() {
+    public List<ShipmentRecord> getAllShipments() {
         return service.getAllShipments();
     }
 
     @GetMapping("/{id}")
-    public ShipmentRecordEntity getShipmentById(@PathVariable Long id) {
+    public ShipmentRecord getShipmentById(@PathVariable Long id) {
         return service.getShipmentById(id);
     }
 
     @GetMapping("/code/{code}")
-    public ShipmentRecordEntity getShipmentByCode(@PathVariable String code) {
+    public ShipmentRecord getShipmentByCode(@PathVariable String code) {
         return service.getShipmentByCode(code);
     }
 
