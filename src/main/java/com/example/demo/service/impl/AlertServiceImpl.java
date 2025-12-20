dcd62ec6730db4556bcf67408a11ad99342fed11
bala-amypo <bala@amypo.in>
2025-12-20 16:02:22 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/AlertServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AlertServiceImpl.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.demo.entity.AlertRecordEntity;
+import com.example.demo.entity.AlertRecord;
 import com.example.demo.repository.AlertRecordRepository;
 import com.example.demo.service.AlertService;
 
@@ -16,22 +16,22 @@ public class AlertServiceImpl implements AlertService {
     private AlertRecordRepository repository;
 
     @Override
-    public AlertRecordEntity addAlert(AlertRecordEntity alert) {
+    public AlertRecord addAlert(AlertRecord alert) {
         return repository.save(alert);
     }
 
     @Override
-    public List<AlertRecordEntity> getAllAlerts() {
+    public List<AlertRecord> getAllAlerts() {
         return repository.findAll();
     }
 
     @Override
-    public AlertRecordEntity getAlertById(Long id) {
+    public AlertRecord getAlertById(Long id) {
         return repository.findById(id).orElse(null);
     }
 
     @Override
-    public List<AlertRecordEntity> getAlertsByShipmentId(Long shipmentId) {
+    public List<AlertRecord> getAlertsByShipmentId(Long shipmentId) {
         return repository.findByShipmentId(shipmentId);
     }
 
