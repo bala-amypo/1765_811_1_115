8cc132a0731b8affc07bf33db374ca78710107f8
bala-amypo <bala@amypo.in>
2025-12-20 16:01:03 +0000

    User commit

--- src/main/java/com/example/demo/repository/ShipmentRecordRepository.java
+++ src/main/java/com/example/demo/repository/ShipmentRecordRepository.java
@@ -2,11 +2,11 @@ package com.example.demo.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import com.example.demo.entity.ShipmentRecordEntity;
+import com.example.demo.entity.ShipmentRecord;
 
 @Repository
 public interface ShipmentRecordRepository
-        extends JpaRepository<ShipmentRecordEntity, Long> {
+        extends JpaRepository<ShipmentRecord, Long> {
 
-    ShipmentRecordEntity findByShipmentCode(String shipmentCode);
+    ShipmentRecord findByShipmentCode(String shipmentCode);
 }
