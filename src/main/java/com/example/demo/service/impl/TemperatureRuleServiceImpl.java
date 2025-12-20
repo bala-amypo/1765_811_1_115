253b61959578928c0c5d8f90dd784412778f7c00
bala-amypo <bala@amypo.in>
2025-12-20 16:04:38 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/TemperatureRuleServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/TemperatureRuleServiceImpl.java
@@ -5,7 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.example.demo.entity.TemperatureRuleEntity;
+import com.example.demo.entity.TemperatureRule;
 import com.example.demo.repository.TemperatureRuleRepository;
 import com.example.demo.service.TemperatureRuleService;
 
@@ -16,17 +16,17 @@ public class TemperatureRuleServiceImpl implements TemperatureRuleService {
     private TemperatureRuleRepository repository;
 
     @Override
-    public TemperatureRuleEntity saveRule(TemperatureRuleEntity rule) {
+    public TemperatureRule saveRule(TemperatureRule rule) {
         return repository.save(rule);
     }
 
     @Override
-    public List<TemperatureRuleEntity> getActiveRules() {
+    public List<TemperatureRule> getActiveRules() {
         return repository.findByActiveTrue();
     }
 
     @Override
-    public List<TemperatureRuleEntity> getRulesByProductType(String productType) {
+    public List<TemperatureRule> getRulesByProductType(String productType) {
         return repository.findByProductType(productType);
     }
 }
