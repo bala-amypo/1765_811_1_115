96979eac441d990da65d57e2567dcba150451b20
bala-amypo <bala@amypo.in>
2025-12-20 15:53:26 +0000

    User commit

--- src/main/java/com/example/demo/entity/AlertRecord.java
+++ src/main/java/com/example/demo/entity/AlertRecord.java
@@ -6,7 +6,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
-public class AlertRecordEntity {
+public class AlertRecord {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
