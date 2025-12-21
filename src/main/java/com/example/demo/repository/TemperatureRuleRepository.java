14f22e6c6990b146baa2f8b4f0dcb0d3854f2b7c
bala-amypo <bala@amypo.in>
2025-12-21 06:58:35 +0000

    User commit

--- src/main/java/com/example/demo/repository/TemperatureRuleRepository.java
+++ src/main/java/com/example/demo/repository/TemperatureRuleRepository.java
@@ -16,6 +16,7 @@
 package com.example.demo.repository;
 
 import java.time.LocalDate;
+import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
