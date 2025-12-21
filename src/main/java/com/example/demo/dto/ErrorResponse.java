aaddc3a95b4ac231cfc23bdb0cdce0d6aa781d5d
bala-amypo <bala@amypo.in>
2025-12-21 06:03:52 +0000

    User commit

--- src/main/java/com/example/demo/dto/ErrorResponse.java
+++ src/main/java/com/example/demo/dto/ErrorResponse.java
@@ -1,40 +1,40 @@
-// package com.example.demo.dto;
+package com.example.demo.dto;
 
-// import java.time.LocalDateTime;
+import java.time.LocalDateTime;
 
-// public class ErrorResponse {
+public class ErrorResponse {
 
-//     private LocalDateTime timestamp;
-//     private int status;
-//     private String error;
-//     private String message;
-//     private String path;
+    private LocalDateTime timestamp;
+    private int status;
+    private String error;
+    private String message;
+    private String path;
 
-//     public ErrorResponse(int status, String error, String message, String path) {
-//         this.timestamp = LocalDateTime.now();
-//         this.status = status;
-//         this.error = error;
-//         this.message = message;
-//         this.path = path;
-//     }
+    public ErrorResponse(int status, String error, String message, String path) {
+        this.timestamp = LocalDateTime.now();
+        this.status = status;
+        this.error = error;
+        this.message = message;
+        this.path = path;
+    }
 
-//     public LocalDateTime getTimestamp() {
-//         return timestamp;
-//     }
+    public LocalDateTime getTimestamp() {
+        return timestamp;
+    }
 
-//     public int getStatus() {
-//         return status;
-//     }
+    public int getStatus() {
+        return status;
+    }
 
-//     public String getError() {
-//         return error;
-//     }
+    public String getError() {
+        return error;
+    }
 
-//     public String getMessage() {
-//         return message;
-//     }
+    public String getMessage() {
+        return message;
+    }
 
-//     public String getPath() {
-//         return path;
-//     }
-// }
+    public String getPath() {
+        return path;
+    }
+}
