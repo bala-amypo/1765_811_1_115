71e3e6c82306ebd710f4ce36344760551ff2eab0
bala-amypo <bala@amypo.in>
2025-12-25 18:13:50 +0000

    User commit

--- src/main/java/com/example/demo/repository/TemperatureRuleRepository.java
+++ src/main/java/com/example/demo/repository/TemperatureRuleRepository.java
@@ -3,9 +3,8 @@ package com.example.demo.repository;
 import com.example.demo.entity.TemperatureRule;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
@@ -14,14 +13,9 @@ public interface TemperatureRuleRepository
 
     List<TemperatureRule> findByActiveTrue();
 
-    @Query("""
-        SELECT r FROM TemperatureRule r
-        WHERE r.productType = :productType
-        AND :temperature BETWEEN r.minTemp AND r.maxTemp
-        AND r.active = true
-    """)
-    TemperatureRule findApplicableRule(
-            @Param("productType") String productType,
-            @Param("temperature") Double temperature
+    List<TemperatureRule> findByProductTypeAndEffectiveFromLessThanEqualAndEffectiveToGreaterThanEqual(
+            String productType,
+            LocalDate from,
+            LocalDate to
     );
 }
