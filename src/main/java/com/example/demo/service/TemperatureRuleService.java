cc6c016fcf631be0ae1cc31da1754bb6931a80a6
bala-amypo <bala@amypo.in>
2025-12-26 03:55:25 +0000

    User commit

--- src/main/java/com/example/demo/service/TemperatureRuleService.java
+++ src/main/java/com/example/demo/service/TemperatureRuleService.java
@@ -1,6 +1,6 @@
 package com.example.demo.service;
 
-import com.example.demo.entity.TemperatureRule;
+import com.example.demo.model.TemperatureRule;
 import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
