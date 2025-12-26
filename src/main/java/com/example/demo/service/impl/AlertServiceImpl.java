56335412436b273e2da922026686c4596c62a9e4
bala-amypo <bala@amypo.in>
2025-12-26 14:51:34 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/AlertServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/AlertServiceImpl.java
@@ -4,9 +4,10 @@ package com.example.demo.service.impl;
 import com.example.demo.entity.AlertRecord;
 import com.example.demo.repository.AlertRecordRepository;
 import com.example.demo.service.AlertService;
-
+import org.springframework.stereotype.Service;
 import java.util.List;
 
+@Service
 public class AlertServiceImpl implements AlertService {
 
     private final AlertRecordRepository repo;
--- src/main/java/com/example/demo/service/impl/ShipmentRecordServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/ShipmentRecordServiceImpl.java
@@ -5,10 +5,11 @@ import com.example.demo.entity.ShipmentRecord;
 import com.example.demo.exception.ResourceNotFoundException;
 import com.example.demo.repository.ShipmentRecordRepository;
 import com.example.demo.service.ShipmentRecordService;
-
+import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class ShipmentRecordServiceImpl implements ShipmentRecordService {
 
     private final ShipmentRecordRepository repo;
