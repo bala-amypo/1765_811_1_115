82497db8cf669843a255166c8881852ae74e1253
bala-amypo <bala@amypo.in>
2025-12-26 04:01:25 +0000

    User commit

--- src/main/java/com/example/demo/model/AlertRecord.java
+++ src/main/java/com/example/demo/model/AlertRecord.java
@@ -1,9 +1,10 @@
 package com.example.demo.model;
 
-import jakarta.persistence.model;
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
 public class AlertRecord {
--- src/main/java/com/example/demo/model/BreachRecord.java
+++ src/main/java/com/example/demo/model/BreachRecord.java
@@ -1,10 +1,6 @@
 package com.example.demo.model;
 
-import jakarta.persistence.model;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-
+import jakarta.persistence.*;
 @Entity
 public class BreachRecord {
 
